f3a4b770a5518b2e11d1dab1a642e19435e8afc8
shivani jadhav <shivani.jadhav@techdenovo.com>
2021-07-14 13:05:10 -0700

    variable added

--- Animal.java
+++ Animal.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
     System.out.println("this is a animal class");
     System.out.println("this is a animal class in practice  ");
     System.out.println("modify the class");
-
+private int a;
 
 }
 
