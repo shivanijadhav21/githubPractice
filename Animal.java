e0376cdcd929f455b85efd7426fda8499bd97672
shivani jadhav <shivani.jadhav@techdenovo.com>
2021-07-14 12:10:00 -0700

    file created

---
+++ Animal.java
@@ -0,0 +1,14 @@
+class Animal{
+
+    int a;
+    int b;
+
+public static void main(String[] args) {
+    
+    System.out.println("this is a animal class");
+    System.out.println("this is a animal class in practice  ");
+    
+
+}
+
+}
